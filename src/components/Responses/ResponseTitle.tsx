c5ec30e2d44b235acb84f4d409a3aa67f18cfda1
bellonline <bell_extrasensory@hotmail.com>
2020-12-22 15:27:25 +0700

    redoc upload

---
+++ src/components/Responses/ResponseTitle.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+import { ShelfIcon } from '../../common-elements';
+import { Markdown } from '../Markdown/Markdown';
+
+export interface ResponseTitleProps {
+  code: string;
+  title: string;
+  type: string;
+  empty?: boolean;
+  opened?: boolean;
+  className?: string;
+  onClick?: () => void;
+}
+
+export class ResponseTitle extends React.PureComponent<ResponseTitleProps> {
+  render() {
+    const { title, type, empty, code, opened, className, onClick } = this.props;
+    return (
+      <button
+        className={className}
+        onClick={(!empty && onClick) || undefined}
+        aria-expanded={opened}
+        disabled={empty}
+      >
+        {!empty && (
+          <ShelfIcon
+            size={'1.5em'}
+            color={type}
+            direction={opened ? 'down' : 'right'}
+            float={'left'}
+          />
+        )}
+        <strong>{code} </strong>
+        <Markdown compact={true} inline={true} source={title} />
+      </button>
+    );
+  }
+}
